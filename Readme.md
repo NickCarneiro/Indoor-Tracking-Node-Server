b5d6b2dd5423cca19c10d37ee8283c8f5b75b28b
Nick Carneiro <burthawk101@gmail.com>
2011-09-29 22:46:41 -0500

    initial commit

---
+++ Readme.md
@@ -0,0 +1,27 @@
+
+# Real Time Indoor Navigation for Android Phones
+
+# Overview
+ * Send JSON-encoded post requests to /coordinates
+ * Data must look like this:
+	
+	{
+		x:	35,
+	 	y:	23,
+		device_id: "android-23"
+	};
+
+
+
+## Requirements
+  * [node](https://github.com/joyent/node)
+  * [npm](https://github.com/isaacs/npm)
+  * socket.io
+
+
+# License
+
+Several components to H5EB have different licenses, (e.g. components from [H5BP](https://github.com/paulirish/html5-boilerplate)).
+
+All other work is [The WTFPL](http://sam.zoy.org/wtfpl/)
+
---
+++ public/javascripts/single-tile.js
@@ -0,0 +1,100 @@
+
+var map = new OpenLayers.Map({
+    div: "mapDiv",
+    layers: [
+        new OpenLayers.Layer.Image(
+            "Single Tile", 
+            "floorplan.jpg",
+            new OpenLayers.Bounds(-360, -180, 360, 180),
+            new OpenLayers.Size(425, 318)
+        )
+    ],
+    center: new OpenLayers.LonLat(6.5, 40.5),
+    zoom: 1
+});
+
+map.addControl(new OpenLayers.Control.LayerSwitcher());
+
+ var markers = new OpenLayers.Layer.Markers( "Markers" );
+    map.addLayer(markers);
+  
+   var x = 0;
+    var y = 0;
+    var x_vel = 20;
+    var y_vel = 20;
+  var marker = new OpenLayers.Marker(new OpenLayers.LonLat(x, y));
+    markers.addMarker(marker);
+
+    //setInterval(moveMarker, 100);
+
+    function generateCoordinates(){
+        
+
+        if(x >= 360 || x <= -360){
+            x_vel = x_vel * -1;
+        } 
+
+        if(y >= 180 || y <= -180){
+            y_vel = y_vel * -1;
+        } 
+        x += x_vel;
+        y += y_vel;
+
+        var json = {
+            x:          x,
+            y:          y,
+            device_id: "gumbo"
+        };
+        //console.log("posting: " + json.x);
+        $.post("/coordinates", json, function(data){
+            //console.log(data);
+        });
+    }
+
+
+    //called on map page to draw marker
+    function setCoordinates(x, y, device_id){
+        marker.moveTo( 
+                map.getLayerPxFromViewPortPx(
+                    map.getPixelFromLonLat(
+                        new OpenLayers.LonLat(x, y)
+                    )
+                )
+            );
+        $("#coordinates").text("x: " + x + " y: " + y);
+    }
+    $(function(){
+        var interval_id;
+        $("#move_marker").click(function(){
+           marker.draw();
+            x = $("#x_pos").val();
+            y = $("#y_pos").val();
+            marker.moveTo( 
+                map.getLayerPxFromViewPortPx(
+                    map.getPixelFromLonLat(
+                        new OpenLayers.LonLat(x, y)
+                    )
+                )
+            );
+            $("#coordinates").text("x: " + x + " y: " + y);
+        });
+
+        $("#start_fakephone").click(function(){
+           interval_id = setInterval(generateCoordinates, 200); 
+           $("#coordinate_list").append("<li>Generating Coordinates</li>");
+        });
+        $("#stop_fakephone").click(function(){
+           clearInterval(interval_id); 
+           $("#coordinate_list").append("<li>Stopping Coordinates</li>");
+        });
+
+
+
+        var socket = io.connect('http://trillworks.com:3010');
+        
+        socket.on('coordinates', function (data) {
+            //console.log("data returned: " + data);
+            //console.log("coordinates event: " + data.x);
+            setCoordinates(data.x, data.y, data.device_id);
+        });
+    });
